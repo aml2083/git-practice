a7101c9dfb233520649bafa689a7af6195ed2cab
Anthony Le <aml2083@gmail.com>
2019-05-08 10:00:14 -0400

    Angular Spring Boot JWT Authentication

---
+++ SpringBootJwtAuthentication/src/main/java/com/le/jwtauthentication/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.le.jwtauthentication.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.le.jwtauthentication.model.Role;
+import com.le.jwtauthentication.model.RoleName;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
